51fc905173fff68f5dd32be72b1b194c9e326866
francisadoche5 <francisadoche5@gmail.com>
2026-06-16 04:58:39 +0100

    Create assets.js

---
+++ frontend/src/config/assets.js
@@ -0,0 +1,13 @@
+const CDN = 'https://cdn.jsdelivr.net/gh/francisadoche5/talon-clash@main';
+
+const ASSETS = {
+  battle: {
+    victoryHeader: `${CDN}/assets/battle/file_00000000c16471f498d60fa3da37e61d.png`,
+    defeatHeader:  `${CDN}/assets/battle/file_0000000037cc71f481c27f128ad8b1f3.png`,
+  },
+  icons: {},
+  ui: {},
+  sprites: {},
+};
+
+export default ASSETS;
