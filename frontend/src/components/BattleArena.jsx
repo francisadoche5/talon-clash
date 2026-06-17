73caef86542134bed1f843af50e91b3ad1fe4b6c
francisadoche5 <francisadoche5@gmail.com>
2026-06-17 11:48:38 +0100

    Update BattleArena.jsx

--- frontend/src/components/BattleArena.jsx
+++ frontend/src/components/BattleArena.jsx
@@ -433,7 +433,7 @@ export default function BattleArena({ player, result, onClose }) {
                 style={{
                   /* Push image up above the card top.
                      Adjust this number if you want more/less of the image above the ribbon. */
-                  top: -125,
+                  top: -165,
                   zIndex: 40,
                 }}>
                 <img
