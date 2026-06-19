b9579669ccae519cbb8801778aec13d2a239a75d
francisadoche5 <francisadoche5@gmail.com>
2026-06-19 06:26:23 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -29,4 +29,5 @@ export const upgradeSkill    = (telegramId, skillType)     => API.post('/api/ski
 export const getPublicConfig = ()                          => API.get('/api/config');
 export const createInvoice   = (telegram_id, product)      => API.post('/api/payments/invoice',    { telegram_id, product });
 export const purchaseProduct = (telegram_id, product)      => API.post('/api/payments/purchase',   { telegram_id, product });
+export const useBooster      = (telegramId, type)          => API.post(`/api/players/${telegramId}/use-booster`, { type });
 export const devBoost        = (telegramId, secret)         => API.post(`/api/dev/boost/${telegramId}`, { secret });
